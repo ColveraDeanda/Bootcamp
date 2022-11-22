15cc1c9ffaec7a6f4a453a3c1457eeaa7e5222f1
Olvera Deanda <colveradeanda@deloitte.com>
2022-11-21 23:44:18 -0600

    Modal Changes

--- LWC/force-app/main/default/lwc/discountComponent/discountComponent.js
+++ LWC/force-app/main/default/lwc/discountComponent/discountComponent.js
@@ -1,12 +1,20 @@
 import { LightningElement, track, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import getStations from '@salesforce/apex/discountController.getAllStations';
+import getAccountFleet from '@salesforce/apex/discountController.getAccountFleet';
+import getStationsByState from '@salesforce/apex/discountController.getStationsByState';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const RECORD_FIELDS_OPPTY = [
     'Opportunity.Account',
     'Opportunity.Name',
     'Opportunity.OwnerId'
 ];
+
+const RECORD_FIELDS_FLEET = [
+    'Fleet__c.State__c',
+    'Fleet__c.National_Fleet__c'
+]
 
 const STATION_TABLE_COLUMNS = [
     {label: 'Station Number', fieldName: 'Station_Number__c'},
@@ -17,12 +25,21 @@ const STATION_TABLE_COLUMNS = [
 
 export default class DiscountComponent extends LightningElement {
 
+    /**
+     * Usar siempre que sea posible getRecord, se podrá usar sin problema siempre y cuando se cuente con el ID del objeto
+     * En caso de no contar con el ID en nigún lado, solo así hacer la llamada Apex.
+     */
 
-    @api recordId;
+
+    @api recordId; // Opportunity
     @track isModalOpen = false;
     opptyRecords = {'sobjectType': 'Opportunity'}; // Objeto de tipo Opportunity.
     stationData; // Station Records
     stationColumns = STATION_TABLE_COLUMNS;
+    selectedFleetId;
+    tempStations;
+    @track accountFleet; // Account related to Fleet.
+    stationsSelected = [];
 
 
     connectedCallback() {
@@ -33,7 +50,6 @@ export default class DiscountComponent extends LightningElement {
     getOpptyRecordFields({data, error}) {
         if(data) {
             this.opptyRecords.Name = data.fields.Name.value;
-           // this.opptyRecords.Account = data.fields.Account.value;;
             this.opptyRecords.Account = data.fields.Account.value.__ref.split(':')[3];
             this.opptyRecords.OwnerId = data.fields.OwnerId.value;
             error = undefined;
@@ -43,24 +59,101 @@ export default class DiscountComponent extends LightningElement {
         }
     }
 
+    
     @wire(getStations, {})
         wiredStations({data, error}) {
             if(data) {
-                console.log('Station Data:', data);
-                this.stationData = data;
+                this.tempStations = data;
                 error = undefined
             } else if(error) {
                 console.log(error);
                 data = undefined;
             }
         }
+        
+
+    
+    @wire(getRecord, {recordId: '$selectedFleetId', fields: RECORD_FIELDS_FLEET})
+    wiredFleet({data, error}) {
+        if(data) {
+            console.log(data);
+            if(data.fields.National_Fleet__c.value) {
+                this.stationData = this.tempStations;
+            } else {
+                const state = data.fields.State__c.value;
+                let stations = this.tempStations;
+                let lstStations = [];
+
+                stations.forEach(element => {
+                    if(element.State__c === state) {
+                        lstStations.push(element);
+                    }
+                });
+                this.stationData = lstStations;
+            }
+            
+            error = undefined
+        } else if(error) {
+            console.log(error);
+            data = undefined;
+        }
+    }
     
 
     
     
-    handleSelectedFleet() {
-        // Retreiving all Station depending fleet selection.
+    handleSelectedFleet(event) {
+        console.log('aqui');
+        this.selectedFleetId = event.detail.value[0];
+        if(this.selectedFleetId) {
+            getAccountFleet({ fleetId: this.selectedFleetId })
+            .then(res =>  {
+                this.accountFleet = res[0].Account__c;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        } else {
+            this.stationData = undefined;
+        } 
         
+    }
+
+    handleSuccess(event) {
+        console.log('Discount Program ID: ', event.detail.id);
+        this.dispatchEvent(new ShowToastEvent({
+            title: 'Success',
+            message: 'New discount program has been created successfully.',
+            variant: 'success'
+        }));
+        this.closeModal();
+    }
+
+    handleRowSelection(event) {
+        let selectedRowsDetail = event.detail.selectedRows;
+        let selectedIdsArray = [];
+
+        for (const element of selectedRowsDetail) {
+            //console.log('elementid', element.Id);
+            selectedIdsArray.push(element.Id);
+        }
+
+        console.log(JSON.parse(JSON.stringify(selectedIdsArray)));
+    }
+
+    getSelectedRec() {
+        var selectedRecords =  this.template.querySelector("lightning-datatable").getSelectedRows();
+      if(selectedRecords.length > 0){
+          console.log('selectedRecords are ', selectedRecords);
+ 
+          let ids = '';
+          selectedRecords.forEach(currentItem => {
+              ids = ids + ',' + currentItem.Id;
+          });
+          this.selectedIds = ids.replace(/^,/, '');
+          this.lstSelectedRecords = selectedRecords;
+          console.log(this.selectedIds);
+      }  
     }
 
     openModal() {
@@ -68,9 +161,7 @@ export default class DiscountComponent extends LightningElement {
     }
     closeModal() {
         this.isModalOpen = false;
-    }
-    submitDetails() {
-        this.isModalOpen = false;
+        this.accountFleet = undefined;
     }
 
 }
